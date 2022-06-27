affdbae823d5c1e0791edeea7c5954207e932fff
Stanislau Marudau <marudov.stas@gmail.com>
2022-06-27 16:55:20 +0300

    Fourth try of the task

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -2,19 +2,16 @@ package lang.print.gaps.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        int first;
-        int second;
-        int third;
-        first = 1;
-        second = 10;
-        third = 100;
+        int first=1;
+        int second=10;
+        int third=100;
         System.out.print(first+"\n"+second+"\n"+third+"\n");
-        first =15;
-        second =6;
-        third =4;
         int linkToFirst = first;
         int linkToSecond =  second;
         int linkToThird = third;
+        first =15;
+        second =6;
+        third =4;
         System.out.print(first+"\n"+second+"\n"+third+"\n"+linkToFirst+"\n"+linkToSecond+"\n"+linkToThird+"\n");
 
     }
