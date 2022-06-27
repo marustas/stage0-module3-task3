0da02e650a329732737a5fb7c149985443b961c4
Stanislau Marudau <marudov.stas@gmail.com>
2022-06-27 17:10:59 +0300

    Purrrfection

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -2,9 +2,12 @@ package lang.print.gaps.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        int first = 1;
-        int second = 10;
-        int third = 100;
+        int first;
+        int second;
+        int third;
+        first = 1;
+        second = 10;
+        third = 100;
         System.out.print(first+"\n"+second+"\n"+third+"\n");
     }
 }
